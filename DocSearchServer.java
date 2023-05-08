143430a08fe4cd0547ea3a990157a1b8910c7a3e
nnorstad <nnorstad@ucsd.edu>
2023-05-07 19:04:03 -0700

    pee

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -69,7 +69,7 @@ class DocSearchServer {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
+        
         int port = Integer.parseInt(args[0]);
 
         Server.start(port, new Handler(args[1]));
